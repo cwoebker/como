c4e1fc73a202783780b2f953eed74c462e840eac
Cecil Wöbker <me@cwoebker.com>
2018-08-28 21:25:41 +0200

    Bump version number to 0.6.1

--- como/__init__.py
+++ como/__init__.py
@@ -5,6 +5,6 @@ como: batteries complete
 
 __title__ = 'como'
 __author__ = 'cwoebker'
-__version__ = '0.6.0'
+__version__ = '0.6.1'
 __licence__ = 'BSD'
-__copyright__ = '(c) 2012-2018 Cecil Woebker'
+__copyright__ = '© 2012-2018 Cecil Wöbker'
