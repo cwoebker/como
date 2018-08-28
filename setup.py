abcd27fad0e9b9b48d151f142e769a5a3be77577
Cecil Wöbker <me@cwoebker.com>
2018-08-28 21:33:53 +0200

    Update setup.py file

--- setup.py
+++ setup.py
@@ -1,61 +1,141 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pip install twine
+# Note: also necessary for building the code:
+#   $ pip install wheel
+
+import io
 import os
 import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, Command
 
 import como
+
+# Package meta-data.
+NAME = como.__name__
+DESCRIPTION = 'como: batteries complete'
+URL = 'https://github.com/cwoebker/como'
+EMAIL = 'me@cwoebker.com'
+AUTHOR = como.__author__
+REQUIRES_PYTHON = '>=2.7.0'
+VERSION = como.__version__
+LICENSE = como.__license__
+
+# What packages are required for this module to be executed?
+REQUIRED = [
+    'paxo', 'python-dateutil', 'python-crontab', 'tablib', 'requests',
+]
+
+# What packages are optional?
+EXTRAS = {
+    # 'fancy feature': ['django'],
+}
+
+# The rest you shouldn't have to touch too much :)
+# ------------------------------------------------
+# Except, perhaps the License and Trove Classifiers!
+# If you do change the License, remember to change the Trove Classifier for that!
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
 try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
 
-# Grab requirments.
-with open('reqs.txt') as f:
-    required = f.readlines()
-
-tests_require = ['nose']
-
-settings = dict()
-
-
-# Publish Helper.
-if sys.argv[-1] == 'publish':
-    os.system('python setup.py sdist upload')
-    sys.exit()
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    with open(os.path.join(here, NAME, '__version__.py')) as f:
+        exec (f.read(), about)
+else:
+    about['__version__'] = VERSION
 
 
-settings.update(
-    name='como',
-    version=como.__version__,
-    description='como: batteries complete',
-    long_description=open('README.md').read(),
-    author=como.__author__,
-    author_email='me@cwoebker.com',
-    url='como.cwoebker.com',
-    download_url='http://github.com/cwoebker/como',
-    license=como.__licence__,
-    packages=['como'],
-    install_requires=required,
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git push --tags')
+
+        sys.exit()
+
+
+# Where the magic happens:
+setup(
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=('tests',)),
+    # If your package is a single module, use this instead of 'packages':
+    # py_modules=['mypackage'],
     entry_points={
-        'console_scripts': [
-            'como = como.cli:main',
-        ],
+        'console_scripts': ['como = como.cli:main'],
     },
-    classifiers=(
-        'Development Status :: 5 - Production/Stable',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators',
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    include_package_data=True,
+    license=LICENSE,
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
         'License :: OSI Approved :: BSD License',
+        'Environment :: Console',
         'Natural Language :: English',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
         'Topic :: Software Development',
         'Topic :: Terminals',
         'Topic :: Utilities',
-    ),
+        'Topic :: Text Processing',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+    # $ setup.py publish support.
+    cmdclass={
+        'upload': UploadCommand,
+    },
 )
-
-setup(**settings)
